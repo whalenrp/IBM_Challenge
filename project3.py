becaa9180456513bc7bb93a5e7a3c147ea066590
Richard Whalen <richard.p.whalen@vanderbilt.edu>
2013-10-23 20:14:03 -0500

    Added scaffolding

---
+++ project3.py
@@ -0,0 +1,31 @@
+
+
+def main():
+	"""
+	Main should interpret flags passed to the program (i.e. training_input_file, 
+	human_readable_classifier, machine_readable_classifier and test_input_file) and write output
+	to the specified file (classify_results_file).
+	"""
+
+
+
+def learn():
+	"""
+	Learn should take in a filepath to the training data (given to main
+	by the flag training_input_file) and return an object which can	classify 
+	new data (which will be written to human_readable_classifier and
+	machine_readable_classifier by main).
+	"""
+
+
+def classify():
+	"""
+	Classify should take in a classification object and a filepath to the
+	testing data (given to main by machine_readable_classifier and test_input_file 
+	respectively) and output a string of the line numbers in the testing which 
+	evaluate to true (this will be written to a file specified by classify_results_file by main).
+	"""
+
+
+
+
