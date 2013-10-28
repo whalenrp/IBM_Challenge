86acbc073111dd870df891438938624136eeb547
Richard Whalen <richard.p.whalen@vanderbilt.edu>
2013-10-28 18:27:23 -0500

    Added main functionality for command parsing

--- project3.py
+++ project3.py
@@ -1,13 +1,59 @@
+import sys
 
-
-def main():
+def main(args):
 	"""
 	Main should interpret flags passed to the program (i.e. training_input_file, 
 	human_readable_classifier, machine_readable_classifier and test_input_file) and write output
 	to the specified file (classify_results_file).
 	"""
 
+	" Set defaults for command parameters"
+	variables = {"trainingInputFile" : "trainingInput.txt", "testInputFile" : "testInput.txt",
+		"machineReadable" : True}
 
+	"Define some functions for input processing"
+	"They take as a parameter the current command arg index"
+	"and return the index after they are done processing"
+	def setTrainingInputFile(curArgNum):
+		curArgNum += 1
+		variables["trainingInputFile"] = sys.argv[curArgNum]
+		return curArgNum
+
+	def setHumanReadableClassifier(curArgNum):
+		variables["machineReadable"] = False
+		return curArgNum
+
+	def setMachineReadableClassifier(curArgNum):
+		variables["machineReadable"] = True
+		return curArgNum
+
+	def setTestInputFile(curArgNum):
+		curArgNum += 1
+		variables["testInputFile"] = sys.argv[curArgNum]
+		return curArgNum
+
+	commandMapping = {"-training_input_file": setTrainingInputFile,
+		"-human_readable_classifier": setHumanReadableClassifier,
+		"-machine_readable_classifier": setMachineReadableClassifier,
+		"-test_input_file": setTestInputFile}
+
+	" Loop over the command arguments, setting fields in the variables dictionary." 
+	curArgNum = 1
+	while (curArgNum < len(sys.argv)):
+		if sys.argv[curArgNum] in commandMapping:
+			curArgNum = commandMapping[sys.argv[curArgNum]](curArgNum)
+			curArgNum +=1
+
+		else:
+			print "Invalid command format."
+			print "Valid options are -training_input_file, -human_readable_classifier, "
+			print " -machine_readable_classifier, and -test_input_file"
+			sys.exit(0)
+
+	print "Training Input Filename :", variables["trainingInputFile"]
+	print "Test Input Filename :", variables["testInputFile"]
+	print "MachineReadable? :", variables["machineReadable"]
+		
 
 def learn():
 	"""
@@ -28,4 +74,5 @@ def classify():
 
 
 
-
+if __name__=='__main__':
+	main(sys.argv)
